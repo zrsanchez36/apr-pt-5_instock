61d43bd8f6c198bd44a84a9bea3b8824159fa624
Glenn Montera <glenn.montera@gmail.com>
2023-10-04 13:51:15 -0400

    add files to server folder

---
+++ server/knexfile.js
@@ -0,0 +1,13 @@
+// Import dotenv
+
+require("dotenv").config();
+
+module.exports = {
+  client: "mysql",
+  connection: {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+};
