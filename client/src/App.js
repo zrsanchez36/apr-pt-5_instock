813b0888f678048237c2e0ea0a38a8e2b1b73cc7
nitishmayekar <nitishmayekar333@gmail.com>
2023-10-06 20:24:04 -0400

    Created SCSS for Header, App and Footer..

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Importing components
 import WarehouseList from "./components/WarehouseList/WarehouseList";
 import Inventory from "./components/Inventory/Inventory";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header"
 
 function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={<WarehouseList />} />
         <Route path="/inventory" element={<Inventory />} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
-import InStockLogo from "../../assets/assets/logo/InStock-logo.svg";
-
+import InStockLogo from "../../assets/assets/logo/InStock-Logo.svg";
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
