07f150306591f5e8463f67aa2ed51bf4299996df
Zsoreign <zrsanchez36@gmail.com>
2023-10-15 11:19:06 -0400

    Quick push

--- client/src/components/_editWarehouse/EditWarehouse.js
+++ client/src/components/_editWarehouse/EditWarehouse.js
@@ -5,6 +5,8 @@ import {ReactComponent as EditArrow} from "../../assets/assets/Icons/arrow_back-
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import {useNavigate} from "react-router-dom";
+
 
 function EditWarehouse() {
   const navigate = useNavigate()
