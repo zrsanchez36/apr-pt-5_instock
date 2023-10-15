7fa88f0cc637ebd8816ebcfe09e24d94913f5936
Zsoreign <zrsanchez36@gmail.com>
2023-10-15 01:07:03 -0400

    Quick css changes to add warehouse

--- client/src/components/_editWarehouse/AddWarehouse.js
+++ client/src/components/_editWarehouse/AddWarehouse.js
@@ -1,13 +1,14 @@
 import React from "react";
-import Header from "../Header/Header";
+// import Header from "../Header/Header";
 import AddWarehouseForm from "./AddWarehouseForm";
 import "./editWarehouse.css";
+import {ReactComponent as Arrow} from "../../assets/Icons/arrow_back-24px.svg"
 
 function AddWarehouse() {
   return (
     <section className="edit__warehouse--section">
       <div className="edit__warehouse--header">
-            <h1>Add new Warehouse</h1>
+            <Arrow /><h1>Add new Warehouse</h1>
       </div>
         <div className="edit__warehouse--container">
           <AddWarehouseForm />
